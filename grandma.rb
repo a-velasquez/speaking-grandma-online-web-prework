73d77ee674b7b1d519a177e8964c069e41e2eafd
Alex Velasquez <jtinnesco@gmail.com>
2019-04-16 05:56:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,5 +16,6 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "Hi Nana, how are you?"
-    puts 
+    puts "HUH?! SPEAK UP, SONNY!"
+  else phrase == ""
 end

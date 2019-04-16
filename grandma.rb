211bffbfb3024807824d8a33079443c036c8c5a1
Alex Velasquez <jtinnesco@gmail.com>
2019-04-16 06:03:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "Hi Nana, how are you?"
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   else phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938"
+    "NO, NOT SINCE 1938"
+  end
 end
